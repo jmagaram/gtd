75c9a287ca9b043e7476c7daec6da0b2cf929534
Justin Magaram <justin@magaram.com>
2018-05-10 11:29:03 -0700

    use linq instead of immutable.js

--- src/uniqueId.test.ts
+++ src/uniqueId.test.ts
@@ -1,10 +1,11 @@
 import * as UniqueId from './uniqueId';
-import { Range } from 'immutable';
+import * as Enumerable from 'linq';
 
 test("when create 1000, each instance is unique", () =>
-    expect(
-        Range(1, 10000)
-            .map(() => UniqueId.create())
-            .groupBy(i => i)
-            .every(i => i.count() == 1))
-        .toBe(true));
+    expect(Enumerable
+        .range(1, 1000)
+        .select(i => UniqueId.create())
+        .groupBy(i => i as string)
+        .select(i => i.count())
+        .max())
+        .toBe(1));
