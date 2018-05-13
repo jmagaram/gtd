60822d5384bfb1d89ddbf0f6163cf7a98ec3741f
Justin Magaram <justin@magaram.com>
2018-05-12 23:35:58 -0700

    more lint issues

--- src/linq.ts
+++ src/linq.ts
@@ -17,7 +17,7 @@ export function* map<T, U>(source: Iterable<T>, selector: (t: T) => U) {
 export function reduce<T>(source: Iterable<T>, accumulator: (t1: T, t2: T) => T): T | undefined {
     let total: T | undefined;
     for (const i of source) {
-        if (total == undefined) {
+        if (total === undefined) {
             total = i;
         }
         else {
--- src/partial.ts
+++ src/partial.ts
@@ -1,8 +1,8 @@
 // Partial application of a function that takes a single object-type argument
 export function partial<Args, Fixed extends Partial<Args>, Result>(fn: (args: Args) => Result, fixed: Fixed) {
     type Unspecified = { [P in Exclude<keyof Args, keyof Fixed>]: Args[P] };
-    let result = (args: Unspecified) => {
-        let combined = {};
+    const result = (args: Unspecified) => {
+        const combined = {};
         Object.assign(combined, fixed, args);
         return fn(combined as Args);
     };
