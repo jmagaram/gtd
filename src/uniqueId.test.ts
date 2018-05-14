12822aa897c497de57a2119cec2f87de586a0388
Justin Magaram <justin@magaram.com>
2018-05-14 00:31:11 -0700

    index for all iter funcs

--- src/uniqueId.test.ts
+++ src/uniqueId.test.ts
@@ -1,12 +1,11 @@
-import { init } from './iter/init'
-import { map } from './iter/map'
+import * as Iter from './iter'
 import { pipe } from './pipe';
 import * as UniqueId from './uniqueId';
 
 test("when create 1000, each instance is unique", () => {
     const ids = pipe(
-        () => init({ count: 1000 }),
-        i => map(i, j => UniqueId.create()),
+        () => Iter.init({ count: 1000 }),
+        i => Iter.map(i, j => UniqueId.create()),
         i => Array.from(i));
     const idSet = new Set<string>(ids);
     expect(ids.length).toBe(idSet.size);
