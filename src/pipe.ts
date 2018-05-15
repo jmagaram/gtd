14f422607db92d7e90897c1ed9e0a8771c9b03bc
Justin Magaram <justin@magaram.com>
2018-05-15 10:21:07 -0700

    remove LastOf conditional type, not needed, use overloaded methods instead

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,25 +1,3 @@
-type LastOf<
-    A,
-    B=never,
-    C=never,
-    D=never,
-    E=never,
-    F=never,
-    G=never,
-    H=never,
-    I=never,
-    J=never> =
-    [B] extends [never] ? A :
-    [C] extends [never] ? B :
-    [D] extends [never] ? C :
-    [E] extends [never] ? D :
-    [F] extends [never] ? E :
-    [G] extends [never] ? F :
-    [H] extends [never] ? G :
-    [I] extends [never] ? H :
-    [J] extends [never] ? I :
-    J;
-
 export function pipe<A, B, C, D, E, F, G, H, I, J>(
     a: () => A,
     mapA: (a: A) => B,
@@ -112,30 +90,30 @@ export function pipe<A, B, C=never, D=never, E=never, F=never, G=never, H=never,
     mapG?: (g: G) => H,
     mapH?: (h: H) => I,
     mapI?: (i: I) => J
-): LastOf<A, B, C, D, E, F, G, H, I, J> {
+) {
     if (mapB === undefined) {
-        return mapA(a()) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+        return mapA(a());
     }
     if (mapC === undefined) {
-        return mapB(mapA(a())) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+        return mapB(mapA(a()));
     }
     if (mapD === undefined) {
-        return mapC(mapB(mapA(a()))) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+        return mapC(mapB(mapA(a())));
     }
     if (mapE === undefined) {
-        return mapD(mapC(mapB(mapA(a())))) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+        return mapD(mapC(mapB(mapA(a()))));
     }
     if (mapF === undefined) {
-        return mapE(mapD(mapC(mapB(mapA(a()))))) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+        return mapE(mapD(mapC(mapB(mapA(a())))));
     }
     if (mapG === undefined) {
-        return mapF(mapE(mapD(mapC(mapB(mapA(a())))))) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+        return mapF(mapE(mapD(mapC(mapB(mapA(a()))))));
     }
     if (mapH === undefined) {
-        return mapG(mapF(mapE(mapD(mapC(mapB(mapA(a()))))))) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+        return mapG(mapF(mapE(mapD(mapC(mapB(mapA(a())))))));
     }
     if (mapI === undefined) {
-        return mapH(mapG(mapF(mapE(mapD(mapC(mapB(mapA(a())))))))) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+        return mapH(mapG(mapF(mapE(mapD(mapC(mapB(mapA(a()))))))));
     }
-    return mapI(mapH(mapG(mapF(mapE(mapD(mapC(mapB(mapA(a()))))))))) as LastOf<A, B, C, D, E, F, G, H, I, J>;
+    return mapI(mapH(mapG(mapF(mapE(mapD(mapC(mapB(mapA(a())))))))));
 }
