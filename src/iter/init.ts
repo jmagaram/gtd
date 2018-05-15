7f89fa245c3fd827935e40a294a183044eacd1f6
Justin Magaram <justin@magaram.com>
2018-05-14 20:59:14 -0700

    use generatortoiterable

--- src/iter/init.ts
+++ src/iter/init.ts
@@ -1,10 +1,10 @@
+import { generatorToIteraable } from './generatorToIterable';
+
 export function init(args: { count: number }): Iterable<number> {
     function* items() {
         for (let i: number = 0; i < args.count; i++) {
             yield i;
         }
     }
-    return {
-        [Symbol.iterator]: items
-    }
+    return generatorToIteraable(items);
 }
