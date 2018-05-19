c41d00c411a308825f285be028e8bc5557904a48
Justin Magaram <justin@magaram.com>
2018-05-19 15:25:12 -0700

    take out react text

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,8 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">To Do List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <View.T store={store} />
         <ActionItemList.T store={store} />
       </div>
