dddec24e6c4f2c958dbf1663e6474a9e32a2dbf5
Justin Magaram <justin@magaram.com>
2018-05-21 13:15:18 -0700

    rename map to array

--- src/state/appState.ts
+++ src/state/appState.ts
@@ -1,25 +1,25 @@
 import { ActionTypes } from 'src/actions'
-import * as ActionItemMap from 'src/state/actionItemMap';
+import * as ActionItemArray from 'src/state/actionItemArray';
 import * as CreateForm from 'src/state/createForm';
 import * as View from 'src/state/view'
 
 export interface T {
     readonly view: View.T;
-    readonly actionItems: ActionItemMap.T;
+    readonly actionItems: ActionItemArray.T;
     readonly createForm: CreateForm.T;
 }
 
 export function createDefault(): T {
     return {
         view: View.createDefault(),
-        actionItems: ActionItemMap.createSampleData(),
+        actionItems: ActionItemArray.createSampleData(),
         createForm: CreateForm.createDefault()
     };
 }
 
 export const reducer = (s: T, a: ActionTypes) => {
     return {
-        actionItems: ActionItemMap.reducer(s.actionItems, s.createForm, a),
+        actionItems: ActionItemArray.reducer(s.actionItems, s.createForm, a),
         view: View.reducer(s.view, a),
         createForm: CreateForm.reducer(s.createForm, a)
     };
