8bf21dd205ccba0e1e89d05889dc2a5838981b78
Justin Magaram <justin@magaram.com>
2018-05-22 13:28:06 -0700

    better typing

--- src/seq/exactlyOne.ts
+++ src/seq/exactlyOne.ts
@@ -1,14 +1,17 @@
+import * as Option from '../utility/option'
 import { pipe } from '../utility/pipe'
 import { T as ZeroBasedIndex } from '../utility/zeroBasedIndex'
 import { last } from './last'
 import { mapi_ } from './mapi'
 import { truncate_ } from './truncate'
 
-export function exactlyOne<T>(source: Iterable<T>): T | undefined {
+export function exactlyOne<T extends Option.Some>(source: Iterable<T>): T | undefined {
     return pipe(
         () => source,
         truncate_(2),
-        mapi_((index, item) => [index, item] as [ZeroBasedIndex, T]),
+        mapi_((index, value) => [index, value] as [ZeroBasedIndex, T]),
         last,
-        i => (i !== undefined && i[0] === 0) ? i[1] : undefined)();
+        Option.filter_(i => i[0] === 0),
+        Option.map_(i => i[1])
+    )();
 }
