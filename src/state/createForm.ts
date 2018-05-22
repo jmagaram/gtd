c08d71f1b519ffa34035441658ff80eb8d142a7e
Justin Magaram <justin@magaram.com>
2018-05-21 21:28:33 -0700

    only update root state when a child changed

--- src/state/createForm.ts
+++ src/state/createForm.ts
@@ -16,10 +16,7 @@ export function reducer(s: T, a: ActionTypes): T {
     switch (a.type) {
         case "createForm_cancel": return createDefault();
         case "createForm_submit": return createDefault();
-
-        // this logic should be with the data
         case "createForm_updateText": return { ...s, text: a.payload, canSubmit: a.payload.trim().length > 0 }
-
         default: return s;
     }
 }
